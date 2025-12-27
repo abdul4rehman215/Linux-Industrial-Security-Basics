25537c34737f2c4b997c0069e05955dc034414d5
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-27 14:15:31 +0530

    Create commands.sh

---
+++ lab02-working-with-directories/commands.sh
@@ -0,0 +1,17 @@
+bash
+mkdir my_new_directory
+ls
+
+mkdir Documents/sub_directory
+ls Documents
+
+rmdir my_new_directory
+ls
+
+mkdir old_directory
+mv old_directory new_directory
+ls
+
+mkdir parent_directory
+mv new_directory parent_directory/
+ls parent_directory
