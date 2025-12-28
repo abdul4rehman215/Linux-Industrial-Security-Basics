fa426e362b67f1d982c80c1e3b9d20102abdc16a
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 13:08:49 +0530

    Create commands.sh

---
+++ lab49-using-and-creating-man-pages/commands.sh
@@ -0,0 +1,15 @@
+man ls
+
+nano test_script.sh
+
+chmod +x test_script.sh
+
+nano test_script.1
+
+gzip test_script.1
+
+sudo mv test_script.1.gz /usr/local/share/man/man1/
+
+sudo mandb
+
+man test_script
