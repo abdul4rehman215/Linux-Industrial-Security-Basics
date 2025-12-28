03927573c08935e5c12c4acd24fbac12bfe9120b
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 12:49:56 +0530

    Create commands.sh

---
+++ lab42-mounting-unmounting-filesystems/commands.sh
@@ -0,0 +1,15 @@
+sudo mkdir /mnt/my_mount
+
+ls -ld /mnt/my_mount
+
+lsblk
+
+sudo mount /dev/nvme0n1 /mnt/my_mount
+
+mount | grep my_mount
+
+sudo umount /mnt/my_mount
+
+mount | grep my_mount
+
+lsblk
