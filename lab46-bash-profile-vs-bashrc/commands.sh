d4ef949b58abfbb9c0e2b1c586c79aa20f42068f
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 13:01:14 +0530

    Create commands.sh

---
+++ lab46-bash-profile-vs-bashrc/commands.sh
@@ -0,0 +1,19 @@
+cd ~
+
+ls -a
+
+nano ~/.bash_profile
+
+nano ~/.bashrc
+
+cp ~/.bashrc ~/.bashrc.backup
+
+cp ~/.bash_profile ~/.bash_profile.backup
+
+nano ~/.bashrc
+
+source ~/.bashrc
+
+ll
+
+echo $MY_VAR
