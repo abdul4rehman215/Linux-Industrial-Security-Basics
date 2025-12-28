bd4042e5eac4bed48795dc958175adffc0d7f682
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 12:07:09 +0530

    Create commands.sh

---
+++ lab39-basic-firewall-ufw/commands.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+sudo apt install ufw
+
+sudo ufw enable
+
+sudo ufw allow ssh
+
+sudo ufw allow http
+
+sudo ufw status
+
+sudo ufw status verbose
+
+sudo ufw deny ssh
+
+sudo ufw allow ssh
