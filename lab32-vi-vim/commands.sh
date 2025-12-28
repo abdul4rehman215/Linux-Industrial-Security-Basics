b4e2c30ff0c9122379e4648830bf84fd74cc830a
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 11:54:26 +0530

    Create commands.sh

---
+++ lab32-vi-vim/commands.sh
@@ -0,0 +1,24 @@
+touch file.txt
+
+vi file.txt
+
+# Inside vi:
+# i
+# This is a sample file.
+# Learning vi editor basics.
+# vi is powerful and efficient.
+# Esc
+# :w
+# :q
+
+cat file.txt
+
+vi file.txt
+
+# Inside vi:
+# /vi
+# Esc
+# :%s/vi/vim/g
+# :wq
+
+cat file.txt
