8da10fda70c3e4d976477543595fddf2a517c9ba
Abdul Rehman <abdul4rehman215@gmail.com>
2025-12-28 13:54:02 +0530

    Create commands.sh

---
+++ lab52-network-file-transfers/commands.sh
@@ -0,0 +1,13 @@
+wget https://example.com/
+
+ls
+
+curl https://example.com
+
+curl https://example.com -o webpage.html
+
+nano webpage.html
+
+wget https://speed.hetzner.de/100MB.bin
+
+wget -c https://speed.hetzner.de/100MB.bin
